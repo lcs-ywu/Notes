cd277bdebce27b49e8deae275e3d19f5fe091a3a
James Wu <ywu@lcs.on.ca>
2021-01-19 10:44:45 -0500

    Added Static Properties and Methods, Access Control and Struct Summary

--- StructsPart2.playground/Contents.swift
+++ StructsPart2.playground/Contents.swift
@@ -44,3 +44,47 @@ struct Person1 {
 
 var ed = Person1(name: "Ed")
 ed.familyTree
+
+//Static Properties and Methods
+//You can also ask Swift to share specific properties and methods across all instances of the struct by declaring them as static.
+struct Student {
+    static var classSize = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+let ed1 = Student(name: "Ed")
+let taylor = Student(name: "Taylor")
+print(Student.classSize)
+
+//Access control
+//Access control lets you restrict which code can use properties and methods. This is important because you might want to stop people reading a property directly, for example.
+//We could create a Person struct that has an id property to store their social security number:
+//Once that person has been created, we can make their id be private so you can’t read it from outside the struct – trying to write ed.id simply won’t work.
+//Just use the private keyword
+//Now only methods inside Person can read the id property. For example:
+struct Person2 {
+    private var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
+//Another common option is public, which lets all other code use the property or method.
+
+//Struct Summary
+//You can create your own types using structures, which can have their own properties and methods.
+//You can use stored properties or use computed properties to calculate values on the fly.
+//If you want to change a property inside a method, you must mark it as mutating.
+//Initializers are special methods that create structs. You get a memberwise initializer by default, but if you create your own you must give all properties a value.
+//Use the self constant to refer to the current instance of a struct inside a method.
+//The lazy keyword tells Swift to create properties only when they are first used.
+//You can share properties and methods across all instances of a struct using the static keyword.
+//Access control lets you restrict what code can use properties and methods.
